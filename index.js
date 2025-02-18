e5978988ca9bc7a7433dff387bc4a25c3765a2a0
jiraphatl <jiraphat.luef@dome.tu.ac.th>
2025-02-18 12:30:12 +0700

    lec-5

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const productRoutes = require('./routes/productRoutes');
 
+app.use('/api', productRoutes);
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/api', productRoutes);
 
 // เชื่อมต่อ MongoDB
 mongoose.connect(process.env.MONGO_URI, {
